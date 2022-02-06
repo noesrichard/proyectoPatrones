00268999a79738af44c25c11ab7def21cfa9b043
TigselemaAlex <84351430+TigselemaAlex@users.noreply.github.com>
2022-02-06 16:56:01 -0500

    Repo y servicio de Categoria

--- src/main/java/com/libreria/catalogo/repositorio/MySQL/RepoCategoria.java
+++ src/main/java/com/libreria/catalogo/repositorio/MySQL/RepoCategoria.java
@@ -1,34 +1,111 @@
 package com.libreria.catalogo.repositorio.MySQL;
 
+import com.libreria.catalogo.entidad.Autor;
 import com.libreria.catalogo.entidad.Categoria;
 import com.libreria.compartido.MySQLRepositorio;
 
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 public class RepoCategoria extends MySQLRepositorio<Categoria,String> {
+
+    private String sql;
+    private PreparedStatement ps;
+    private ResultSet rs;
+
     @Override
     public List<Categoria> listar() {
-        return null;
+        List<Categoria> lista = new ArrayList<>();
+        sql = "SELECT * FROM categoria";
+        try {
+            ps = conexion.prepareStatement(sql);
+            rs = ps.executeQuery();
+            while (rs.next()){
+                lista.add(
+                        new Categoria(
+                                rs.getString("id"),
+                                rs.getString("nombre")
+                        )
+                );
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return lista;
     }
 
     @Override
     public Categoria porId(String id) {
-        return null;
+        sql = "SELECT * FROM categoria WHERE id="+id;
+        Categoria categoria = null;
+
+        try {
+            ps = conexion.prepareStatement(sql);
+            rs = ps.executeQuery();
+            while (rs.next()){
+                categoria = new Categoria(rs.getString("id"), rs.getString("nombre"));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return categoria;
     }
 
     @Override
     public Categoria guardar(Categoria entidad) {
-        return null;
+        sql = "INSERT INTO categoria (nombre) VALUES (?)";
+        try {
+            ps = conexion.prepareStatement(sql);
+            ps.executeUpdate();
+            conexion.commit();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        sql = "SELECT * FROM categoria ORDER BY id DESC LIMIT 1";
+        Categoria categoria = null;
+        try {
+            ps = conexion.prepareStatement(sql);
+            rs = ps.executeQuery();
+            while (rs.next()){
+                categoria = new Categoria(
+                        rs.getString("id"),
+                        rs.getString("nombre")
+                );
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return categoria;
     }
 
     @Override
     public Categoria editar(Categoria entidad, String id) {
+        sql = "UPDATE FROM categoria SET nombre="+entidad.getNombre()+" WHERE id="+id;
+        try {
+            ps = conexion.prepareStatement(sql);
+            ps.executeUpdate();
+            conexion.commit();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
         return null;
     }
 
     @Override
     public Categoria eliminar(String id) {
-        return null;
+        Categoria categoria = porId(id);
+        sql = "DELETE FROM categoria WHERE id=" + id;
+        try {
+            ps = conexion.prepareStatement(sql);
+            ps.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return categoria;
     }
 
 
--- src/main/java/com/libreria/catalogo/repositorio/MySQL/RepoLibro.java
+++ src/main/java/com/libreria/catalogo/repositorio/MySQL/RepoLibro.java
@@ -4,12 +4,18 @@ package com.libreria.catalogo.repositorio.MySQL;
 import com.libreria.catalogo.entidad.Libro;
 import com.libreria.compartido.MySQLRepositorio;
 
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.util.List;
 
 public class RepoLibro extends MySQLRepositorio<Libro, String> {
 
+    private String sql;
+    private PreparedStatement ps;
+    private ResultSet rs;
     @Override
     public List<Libro> listar() {
+
         return null;
     }
 
